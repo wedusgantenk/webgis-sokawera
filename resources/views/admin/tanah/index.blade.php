2f19c422368dcdbef90461c49d057fac96f65dcd
Fauzan Taqiyuddin <19102103@ittelkom-pwt.ac.id>
2023-06-01 22:44:47 +0700

    paginate data

--- resources/views/admin/tanah/index.blade.php
+++ resources/views/admin/tanah/index.blade.php
@@ -55,45 +55,11 @@
                             </tbody>
                         </table>
                         <div class="mt-4">
-                            <div class="demo-inline-spacing">
-                                <!-- Basic Pagination -->
-                                <nav aria-label="Page navigation">
-                                    <ul class="pagination">
-                                        <li class="page-item first">
-                                            <a class="page-link" href="javascript:void(0);"><i
-                                                    class="tf-icon bx bx-chevrons-left"></i></a>
-                                        </li>
-                                        <li class="page-item prev">
-                                            <a class="page-link" href="javascript:void(0);"><i
-                                                    class="tf-icon bx bx-chevron-left"></i></a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="javascript:void(0);">1</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="javascript:void(0);">2</a>
-                                        </li>
-                                        <li class="page-item active">
-                                            <a class="page-link" href="javascript:void(0);">3</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="javascript:void(0);">4</a>
-                                        </li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="javascript:void(0);">5</a>
-                                        </li>
-                                        <li class="page-item next">
-                                            <a class="page-link" href="javascript:void(0);"><i
-                                                    class="tf-icon bx bx-chevron-right"></i></a>
-                                        </li>
-                                        <li class="page-item last">
-                                            <a class="page-link" href="javascript:void(0);"><i
-                                                    class="tf-icon bx bx-chevrons-right"></i></a>
-                                        </li>
-                                    </ul>
-                                </nav>
-                                <!--/ Basic Pagination -->
-                            </div>
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination pagination-primary justify-content-center">
+                                    {!! $data->links() !!}
+                                </ul>
+                            </nav>
                         </div>
                     </div>
                 </div>
